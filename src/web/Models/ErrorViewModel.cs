e33dc118424b62870437d0ff5dd3a3d5b380ad32
Parsa Gachkar <parsa.gachkar@azihub.com>
2024-10-21 08:13:50 +0100

    Initialize project

---
+++ src/web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ParsaSampleHelm.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
